60b6ad80a3f26d17a6f7199ead0a82cde21972b7
Nam <nam@Nams-iMac.local>
2020-07-08 09:15:43 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild('mModal') mModal: ElementRef<HTMLDivElement>;
   title = 'demo-modal-swipe-up';
+
+  onSwipeDown() {
+    this.mModal.nativeElement.classList.remove('show');
+  }
+
+  openModal() {
+    this.mModal.nativeElement.classList.add('show');
+  }
 }
